7e0bceb35286b2c6cc4117642da9dc74fabd4309
Chris Smith <chrisronsmith@gmail.com>
2021-04-03 13:59:18 -0400

    prettify

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-import { h, render, Fragment } from 'https://cdn.skypack.dev/preact'
-import { useState } from 'https://cdn.skypack.dev/preact/hooks'
+import { h, render, Fragment } from "https://cdn.skypack.dev/preact";
+import { useState } from "https://cdn.skypack.dev/preact/hooks";
 
-const text = 'Hello my dude! What is happening? I really would like to know what it is that you think is happening, because I am confused. Specifically, I am confused about what is happening. Can you help me my dude? Many thanks, Eric.'
+const text =
+  "Hello my dude! What is happening? I really would like to know what it is that you think is happening, because I am confused. Specifically, I am confused about what is happening. Can you help me my dude? Many thanks, Eric.";
 
-const Characters = ({text, position}) => h(
-  Fragment,
-  {},
-  text.split('').map(
-    char => h('span', {}, char)
-  )
-)
+const Characters = ({ text, position }) =>
+  h(
+    Fragment,
+    {},
+    text.split("").map((char) => h("span", {}, char))
+  );
 
-const Text = ({text}) => {
-  const [position, setPosition] = useState(0)
-  
-  return h(
-    'div', {className: 'text'}, h(Characters, {text, position})
-  )
-}
+const Text = ({ text }) => {
+  const [position, setPosition] = useState(0);
 
-render(
-  h(Text, {text}),
-  document.getElementById('app')
-)
+  return h("div", { className: "text" }, h(Characters, { text, position }));
+};
+
+render(h(Text, { text }), document.getElementById("app"));
