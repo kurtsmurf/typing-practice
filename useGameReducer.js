7d02a7535a271230b7bd85f92e944f60c67c67ad
Chris Smith <chrisronsmith@gmail.com>
2021-04-07 13:38:25 -0400

    Use an object instead of switches in reducer

--- useGameReducer.js
+++ useGameReducer.js
@@ -1,68 +1,42 @@
 import { useReducer } from "https://cdn.skypack.dev/preact/hooks";
-import modes from './modes.js'
-import events from './events.js'
+import modes from "./modes.js";
+import events from "./events.js";
 
 const initialState = {
   mode: modes.PLAYING,
   position: 0,
 };
 
-export default function useGameReducer() {
-  return useReducer(reducer, initialState);
-}
+const transitions = {
+  [modes.PLAYING]: {
+    [events.TYPE_CORRECT_KEY]: (state) => ({
+      ...state,
+      position: state.position + 1,
+    }),
+    [events.TYPE_INCORRECT_KEY]: (state) => ({
+      ...state,
+      mode: modes.LOST,
+    }),
+    [events.REACH_END]: (state) => ({
+      ...state,
+      mode: modes.WON,
+      position: state.position + 1,
+    }),
+    [events.RESET]: (_) => initialState,
+  },
+  [modes.WON]: {
+    [events.RESET]: (_) => initialState,
+  },
+  [modes.LOST]: {
+    [events.RESET]: (_) => initialState,
+  },
+};
 
 function reducer(state, event) {
-  switch (state.mode) {
-    case modes.PLAYING:
-      return playingReducer(state, event);
-    case modes.WON:
-      return wonReducer(state, event);
-    case modes.LOST:
-      return lostReducer(state, event);
-    default:
-      return state;
-  }
+  const transition = transitions[state.mode][event];
+  return transition ? transition(state) : state;
 }
 
-function playingReducer(state, event) {
-  switch (event) {
-    case events.TYPE_CORRECT_KEY:
-      return {
-        ...state,
-        position: state.position + 1,
-      };
-    case events.TYPE_INCORRECT_KEY:
-      return {
-        ...state,
-        mode: modes.LOST,
-      };
-    case events.REACH_END:
-      return {
-        ...state,
-        mode: modes.WON,
-        position: state.position + 1,
-      };
-    case events.RESET:
-      return initialState;
-    default:
-      return state;
-  }
-}
-
-function wonReducer(state, event) {
-  switch (event) {
-    case events.RESET:
-      return initialState;
-    default:
-      return state;
-  }
-}
-
-function lostReducer(state, event) {
-  switch (event) {
-    case events.RESET:
-      return initialState;
-    default:
-      return state;
-  }
+export default function useGameReducer() {
+  return useReducer(reducer, initialState);
 }
