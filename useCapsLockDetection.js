677d0575ffdd3bdfcf77cecf35e52763971d512f
Chris Smith <chrisronsmith@gmail.com>
2021-07-09 18:54:28 -0400

    Fix caps-lock detection... ? ¯\_(ツ)_/¯

--- useCapsLockDetection.js
+++ useCapsLockDetection.js
@@ -1,14 +1,20 @@
-import { useEffect, useState } from "https://cdn.skypack.dev/preact/hooks";
-import { useWindowEventListener } from "./useWindowEventListener.js"
+import { useState } from "https://cdn.skypack.dev/preact/hooks";
+import { useWindowEventListener } from "./useWindowEventListener.js";
 
 export const useCapsLockDetection = () => {
   const [capsLockIsOn, setCapsLockIsOn] = useState(undefined);
 
-  const onKeyDown = (e) => {
-    setCapsLockIsOn(e.getModifierState && e.getModifierState( 'CapsLock' ));
-  };
-  
-  useWindowEventListener("keydown", onKeyDown);
-  
+  useWindowEventListener("keydown", (e) => {
+    // If getModifierState is not defined, don't even try.
+    if (!e.getModifierState) return;
+
+    // We assume that the game was unlocked by pressing a key.
+    if (e.key === "CapsLock") {
+      setCapsLockIsOn(!capsLockIsOn);
+    } else {
+      setCapsLockIsOn(e.getModifierState("CapsLock"));
+    }
+  }, [capsLockIsOn, setCapsLockIsOn]);
+
   return capsLockIsOn;
 };
--- useWindowEventListener.js
+++ useWindowEventListener.js
@@ -1,8 +1,8 @@
 import { useEffect } from "https://cdn.skypack.dev/preact/hooks";
 
-export const useWindowEventListener = (event, listener) => {
+export const useWindowEventListener = (event, listener, deps = []) => {
   useEffect(() => {
     window.addEventListener(event, listener);
     return () => window.removeEventListener(event, listener);
-  }, []);
+  }, deps);
 };
