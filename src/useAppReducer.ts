204c613faae60f1bfa1ac3f8b55e0a1eb0c7bd4f
Chris Smith <chrisronsmith@gmail.com>
2021-09-14 12:38:04 -0400

    Refactor useAppReducer

--- src/useAppReducer.ts
+++ src/useAppReducer.ts
@@ -12,68 +12,60 @@ const sanitizeText = (text: string) =>
     .trim()
     .replaceAll(/[\s\r\n]+/g, " ");
 
-const transitions = (mode: appMode, event: appEvent) =>
-  (state: appState): appState => {
-    switch (mode) {
-      case "GAME": {
-        switch (event.type) {
-          case "EDIT": {
-            return {
-              ...state,
-              mode: "EDIT",
-            };
-          }
-          default: {
-            return state;
-          }
+const saveText = (text: string) => localStorage.setItem("text", text);
+const getSavedText = () => localStorage.getItem("text");
+
+function reducer(state: appState, event: appEvent): appState {
+  switch (state.mode) {
+    case "GAME": {
+      switch (event.type) {
+        case "EDIT": {
+          return {
+            ...state,
+            mode: "EDIT",
+          };
         }
-      }
-      case "EDIT": {
-        switch (event.type) {
-          case "CANCEL": {
-            return {
-              ...state,
-              mode: "GAME",
-            };
-          }
-          case "SAVE": {
-            return {
-              ...state,
-              mode: "GAME",
-              text: sanitizeText(event.text) || state.text,
-            };
-          }
-          default: {
-            return state;
-          }
+        default: {
+          return state;
         }
-      }
-      default: {
-        return state;
       }
     }
-  };
+    case "EDIT": {
+      switch (event.type) {
+        case "CANCEL": {
+          return {
+            ...state,
+            mode: "GAME",
+          };
+        }
+        case "SAVE": {
+          const nextText = sanitizeText(event.text) || state.text;
+          if (nextText !== state.text) saveText(nextText);
+
+          return {
+            ...state,
+            mode: "GAME",
+            text: nextText,
+          };
+        }
+        default: {
+          return state;
+        }
+      }
+    }
+    default: {
+      return state;
+    }
+  }
+}
 
 const initialState: appState = {
   text: "Perfect practice makes perfect.",
   mode: "GAME",
 };
 
-const saveText = (text: string) => localStorage.setItem("text", text);
-const getSavedText = () => localStorage.getItem("text");
-
-function reducer(state: appState, event: appEvent) {
-  const nextState = transitions(state.mode, event)(state);
-
-  if (nextState.text !== state.text) saveText(nextState.text);
-
-  return nextState;
-}
-
 export function useAppReducer() {
   const savedText = getSavedText();
-
   if (savedText) initialState.text = savedText;
-
   return useReducer(reducer, initialState);
 }
